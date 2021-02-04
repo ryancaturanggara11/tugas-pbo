4daa15688354e14814860e833c26988543416750
ryancaturanggara11 <ryancaturanggara@gmail.com>
2021-02-04 15:26:05 +0700

    tugas pbo 2

---
+++ bilangan.java
@@ -0,0 +1,13 @@
+public class bilangan {
+    public static void main(String[] args) {
+        
+        int angka = 2;
+    
+        if ( angka % 2 == 0 )
+            System.out.println(angka + " adalah bilangan genap");
+         else
+            System.out.println(angka + " adalah bilangan ganjil");
+    
+        }
+            
+}
